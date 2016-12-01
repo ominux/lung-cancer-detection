41208c45ace236a894c6ab9e49fdb3e26c743560
FabianFalck <fabian.falck@web.de>
2016-12-01 14:12:29 +0800

    tensorflow version issues - before updating to GPU

--- Code/test4_softmaxReg.py
+++ Code/test4_softmaxReg.py
@@ -26,18 +26,13 @@ import sys
 
 # Import data
 from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 print(mnist.train.next_batch(1))
 
 import tensorflow as tf
 
-FLAGS = None
-
 # def main(_):
-
-mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
 # Create the model
 x = tf.placeholder(tf.float32, [None, 784]) #Placeholders are fixed values (inputs) --> not modifiable
@@ -60,15 +55,23 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-sess = tf.InteractiveSession()
+sess = tf.Session()
 # Train
-tf.global_variables_initializer().run()
+
+
+#sess.run(tf.global_variables_initializer()) before
+sess.run(tf.initialize_all_variables())
+
 for _ in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 # Test trained model
+
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+sess.run(correct_prediction)
+
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                     y_: mnist.test.labels}))
